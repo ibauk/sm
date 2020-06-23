2cb8f5d40cba8a492e6011c19536c62a546dd784
Iron Butt UK <webmaster@ironbutt.co.uk>
2020-06-23 18:05:08 +0100

    Delete smdb_update1_2.sql

--- smdb_update1_2.sql
+++
@@ -1,9 +0,0 @@
--- Upgrade to include Entrant contact details
-
-ALTER TABLE `entrants`	ADD 	`Phone` TEXT;
-ALTER TABLE `entrants`	ADD 	`Email` TEXT;
-ALTER TABLE `entrants`	ADD 	`NoKName` TEXT;
-ALTER TABLE `entrants`	ADD 	`NoKRelation` TEXT;
-ALTER TABLE `entrants`	ADD 	`NoKPhone` TEXT;
-
-UPDATE `rallyparams` SET `DBVersion`=2;
